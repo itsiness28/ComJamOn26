8f53c59609b73e4d8cb72596405ffdc30e3d69d2
Inés Castaño Gijón <inescasgi@outlook.com>
2026-02-21 21:05:02 +0100

    Un huevo de cosas

---
+++ las-moiras-funcionarias/Scenes/minigames_manager.gd
@@ -0,0 +1,18 @@
+extends Node2D
+var minijuegoOn = false
+var modoInspeccion = false
+var faseMinijuego = 0 
+var canDrag = false
+var currentSpeaker = ""
+
+# Called when the node enters the scene tree for the first time.
+
+func _ready():
+	pass
+	if (currentSpeaker == "Rosa_Melano"):
+		pass
+		#Here, i have to spawn with spawn_
+	
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
