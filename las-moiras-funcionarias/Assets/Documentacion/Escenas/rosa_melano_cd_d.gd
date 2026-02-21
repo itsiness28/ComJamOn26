49d2c60ba8f25ea687e3e746bac501b4da9391c8
Inés Castaño Gijón <inescasgi@outlook.com>
2026-02-21 22:51:49 +0100

    DRAG AND DROP MATE

--- las-moiras-funcionarias/Assets/Documentacion/Escenas/rosa_melano_cd_d.gd
+++ las-moiras-funcionarias/Assets/Documentacion/Escenas/rosa_melano_cd_d.gd
@@ -1,6 +1,4 @@
-extends Area2D
-
-
+extends Node2D
 var selected = false
 var mouse_offset = Vector2 (0,0)
 
@@ -12,7 +10,7 @@ func followMouse():
 	position = get_global_mouse_position() + mouse_offset
 
 
-func _on_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
+func _on_area_2d_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
 	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT:
 		if  event.pressed: 
 			mouse_offset = position - get_global_mouse_position()
--- las-moiras-funcionarias/Scenes/Mainmenu_Manager.gd
+++ las-moiras-funcionarias/Scenes/Mainmenu_Manager.gd
@@ -9,11 +9,3 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
-
-
-func _on_button_pressed() -> void:
-	get_tree().change_scene_to_file("res://Scenes/Dialogue.tscn")
-
-
-func _on_button_2_pressed() -> void:
-	get_tree().quit()# Replace with function body.
