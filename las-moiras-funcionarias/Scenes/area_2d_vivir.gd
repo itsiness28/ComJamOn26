c216a95465619ff7a51e78e8131299b27f1d0686
Inés Castaño Gijón <146824804+itsiness28@users.noreply.github.com>
2026-02-22 07:28:55 +0100

    sí

---
+++ las-moiras-funcionarias/Scenes/area_2d_vivir.gd
@@ -0,0 +1,9 @@
+extends Area2D
+
+
+	
+
+
+func _on_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
+	if event is InputEventMouseButton and event.pressed:
+		print("Vivir")
--- las-moiras-funcionarias/Scenes/minigames_manager.gd
+++ las-moiras-funcionarias/Scenes/minigames_manager.gd
@@ -1,18 +1,34 @@
 extends Node2D
-var minijuegoOn = false
+var minijuegoOn = true
 var modoInspeccion = false
 var faseMinijuego = 0 
-var canDrag = false
-var currentSpeaker = ""
+var canDrag = false #Usar si al final pongo el tween
+var currentSpeaker = "Rosa_Melano"
 
-# Called when the node enters the scene tree for the first time.
+#region
+var RosaMelano_CdD = preload("res://Assets/Documentacion/Escenas/Rosa Melano/RosaMelano_CdD.tscn")
+var RosaMelano_DNI = preload("res://Assets/Documentacion/Escenas/Rosa Melano/RosaMelano_DNI.tscn")
+var RosaMelano_PlaveCin = preload("res://Assets/Documentacion/Escenas/Rosa Melano/RosaMelano_PlaveCin.tscn")
+var RosaMelano_Testamento = preload("res://Assets/Documentacion/Escenas/Rosa Melano/RosaMelano_Testamento.tscn")
+
+
+#endregion
 
 func _ready():
-	pass
 	if (currentSpeaker == "Rosa_Melano"):
-		pass
-		#Here, i have to spawn with spawn_
+		var RM_CdD_instance: Node = RosaMelano_CdD.instantiate()
+		add_child(RM_CdD_instance)
+		var RM_DNI_instance: Node = RosaMelano_DNI.instantiate()
+		add_child(RM_DNI_instance)
+		var RM_PlaveCin_instance: Node = RosaMelano_PlaveCin.instantiate()
+		add_child(RM_PlaveCin_instance)
+		var RM_Testamento_instance: Node = RosaMelano_Testamento.instantiate()
+		add_child(RM_Testamento_instance)
+		
 	
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+
+func _on_button_pressed() -> void:
+	modoInspeccion = true
+	if (currentSpeaker == "Rosa_Melano"):
+			pass
+	
