fee035f5f6e1f14c5660d514ae83a2e8c131a812
Inés Castaño Gijón <inescasgi@outlook.com>
2026-02-23 09:50:53 +0100

    juego jam terminado

--- las-moiras-funcionarias/Scenes/area_vivir.gd
+++
@@ -1,5 +0,0 @@
-extends Node2D
-
-func _on_area_2d_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
-	if event is InputEventMouseButton:
-		print(event)
--- las-moiras-funcionarias/Scenes/veredicto.gd
+++ las-moiras-funcionarias/Scenes/veredicto.gd
@@ -16,19 +16,7 @@ func _process(delta: float) -> void:
 
 func _on_area_2d_vivir_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
 	if event is InputEventMouseButton:
-		Dialogic.VAR.ContadorVida = Dialogic.VAR.ContadorVida + 1
-		print((Dialogic.VAR.ContadorVida / 2))
-		
-		if (Dialogic.VAR.currentSpeaker == "Rosa_Melano"):
-			Dialogic.VAR.currentSpeaker	= "Ben_Dover"
-		if (Dialogic.VAR.currentSpeaker == "Ben_Dover"):
-			Dialogic.VAR.currentSpeaker	= "Dolores_Crotal"
-		if (Dialogic.VAR.currentSpeaker == "Dolores_Crotal"):
-			Dialogic.VAR.currentSpeaker	= "Olivia_Lopez"
-		if (Dialogic.VAR.currentSpeaker == "Olivia_Lopez"):
-			Dialogic.VAR.currentSpeaker	= "La_Roca"
-			
-		print(Dialogic.VAR.currentSpeaker)
+		get_tree().change_scene_to_file("res://Scenes/Final.tscn")
 
 
 func _on_area_2d_morir_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
